d6ead4ae116cafde5493a77396386a051c44e836
limzykenneth <limzy.kenneth@gmail.com>
2015-07-07 14:01:47 +0100

    Added filtering function

--- scripts/custom.js
+++ scripts/custom.js
@@ -36,7 +36,7 @@ $(document).ready(function($) {
 		var cid = "c"+id;
 
 		$(this).find('.explanation').html(glossaryGroup.get(cid).toJSON().intro);
-		$(this).find('.explanation').css('display', 'block');
+		$(this).find('.explanation').css('display', 'block').css('text-align','left');
 		glossaryGroup.get(cid).set("expanded","true");
 
 		var w = 430;
@@ -46,7 +46,7 @@ $(document).ready(function($) {
 			w = 200;
 			h = 200;
 			$(this).find('.explanation').html(glossaryGroup.get(cid).toJSON().explain);
-			$(this).find('.explanation').css('display', 'none');
+			$(this).find('.explanation').css('display', 'none').css('text-align','center');
 			glossaryGroup.get(cid).set("expanded","false");
 		}
 		$(this).animate({width: w, height: h}, 100, function(){
@@ -54,12 +54,63 @@ $(document).ready(function($) {
 		});
 	});
 
+	$("#beginner").click(function(e) {
+		showHideTiers(1);
+	});
+	$("#intermediate").click(function(e) {
+		showHideTiers(2);
+	});
+	$("#advanced").click(function(e) {
+		showHideTiers(3);
+	});
+	$("#all").click(function(e) {
+		$(".button").each(function(){
+			animateToShow($(this));
+		});
+	});
+
+	function showHideTiers(index){
+		for (i=0; i < glossaryGroup.length; i++){
+			if (glossaryGroup.at(i).toJSON().tier == index){
+				animateToShow($(".button:eq("+i+")"));
+			}else{
+				animateToHide($(".button:eq("+i+")"));
+			}
+		}
+	}
+
+	function animateToShow($item){
+		$item.animate({
+			width: '200px',
+			height: '200px',
+			padding: '10px',
+			margin: '5px',
+			'font-size': '1em',
+			display: 'block'
+		}, 200, function(){
+			$("#wall").masonry({});
+		});
+	}
+
+	function animateToHide($item){
+		$item.animate({
+			width: '0',
+			height: '0',
+			padding: '0',
+			margin: '0',
+			'font-size': '0em',
+			display: 'none'
+		}, 200, function(){
+			$("#wall").masonry({});
+		});
+	}
+
 	$("#wall").masonry({
 		itemSelector: '.button',
 		columnWidth: 230,
 		isAnimated: true,
 		animationOptions: {
-		    duration: 400
+		    duration: 200
 		}
 	});
 
--- scripts/model.js
+++ scripts/model.js
@@ -13,6 +13,7 @@ futures.glossary = Backbone.Model.extend({
 var iHTML = new futures.glossary({
 	name: "HTML",
 	link: "html",
+	tier: 1,
 	explain: "HTML is a computer language devised to allow website creation.",
 	intro: '<p>HTML is short for "HyperText Markup Language". That may sound scary, but it simply means it is a language for describing web-pages using ordinary text. HTML is not a complex programming language.</p><p>Every web page is actually a HTML file. Each HTML file is just a plain-text file, but with a .html file extension instead of .txt, and is made up of many HTML tags as well as the content for a web page.</p><p>A web site will often contain many html files that link to each other. You can edit HTML files with your favourite editor.</p>',
 	application: "http://www.motherfuckingwebsite.com",
@@ -25,6 +26,7 @@ var iHTML = new futures.glossary({
 var iCSS = new futures.glossary({
 	name: "CSS",
 	link: "css",
+	tier: 1,
 	explain: "CSS is a style sheet language used for describing the look and formatting of a document written in a markup language.",
 	intro: "Ruby is an expressive, dynamic programming language. Ruby on Rails is an open source web framework for building custom web applications. The courses in this section get you quickly up to speed with the basics of the Ruby language and on track to building your first Rails application.",
 	application: "",
@@ -37,6 +39,7 @@ var iCSS = new futures.glossary({
 var iOpenData = new futures.glossary({
 	name: "Open Data",
 	link: "odata",
+	tier: 2,
 	explain: "Open data is data that can be freely used, re-used and redistributed by anyone",
 	intro: "This is intro to Open Data",
 	application: "",
@@ -49,6 +52,7 @@ var iOpenData = new futures.glossary({
 var iJavascript = new futures.glossary({
 	name: "Javascript",
 	link: "js",
+	tier: 1,
 	explain: "An object-oriented computer programming language commonly used to create interactive effects within web browsers.",
 	intro: "JavaScript is the programming language of HTML and the Web.",
 	application: "",
@@ -61,6 +65,7 @@ var iJavascript = new futures.glossary({
 var iRuby = new futures.glossary({
 	name: "Ruby",
 	link: "ruby",
+	tier: 3,
 	explain: "A dynamic, interpreted, open source programming language with a focus on simplicity and productivity",
 	intro: "Ruby is an expressive, dynamic programming language. Ruby on Rails is an open source web framework for building custom web applications. The courses in this section get you quickly up to speed with the basics of the Ruby language and on track to building your first Rails application.",
 	application: "",
@@ -73,6 +78,7 @@ var iRuby = new futures.glossary({
 var iGit = new futures.glossary({
 	name: "Git",
 	link: "git",
+	tier: 2,
 	explain: "Git is the most popular version control system that developers use to track and share code.",
 	intro: "Ruby is an expressive, dynamic programming language. Ruby on Rails is an open source web framework for building custom web applications. The courses in this section get you quickly up to speed with the basics of the Ruby language and on track to building your first Rails application.",
 	application: "",
@@ -85,6 +91,7 @@ var iGit = new futures.glossary({
 var iSwift = new futures.glossary({
 	name: "Swift",
 	link: "swift",
+	tier: 3,
 	explain: "Swift is an innovative new programming language for iOS and OS X with concise yet expressive syntax that produces lightning-fast apps.",
 	intro: "Ruby is an expressive, dynamic programming language. Ruby on Rails is an open source web framework for building custom web applications. The courses in this section get you quickly up to speed with the basics of the Ruby language and on track to building your first Rails application.",
 	application: "",
@@ -97,6 +104,7 @@ var iSwift = new futures.glossary({
 var iIot = new futures.glossary({
 	name: "IoT",
 	link: "iot",
+	tier: 2,
 	explain: "A proposed development of the Internet where everyday objects have network connectivity to send and receive data.",
 	intro: "Ruby is an expressive, dynamic programming language. Ruby on Rails is an open source web framework for building custom web applications. The courses in this section get you quickly up to speed with the basics of the Ruby language and on track to building your first Rails application.",
 	application: "",
